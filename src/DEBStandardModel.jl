2fd98a5e4d691d5638437ea96ca8b8cf122a973f
HenriLaurie <henri.laurie@gmail.com>
2021-12-02 20:14:09 +0200

    small changes to TODO, small bugfixes

--- src/DEBStandardModel.jl
+++ src/DEBStandardModel.jl
@@ -2,26 +2,25 @@ module DEBStandardModel
 
 using DifferentialEquations
 
-struct ShortParams 
-  p 
-end #for getlbDEBstd
+struct ShortParams end #for getlbDEBstd
 
 struct LongParams end #for getlbDEBstd
 
 #TODO
-# getlbDEBstd() which uses
-# scaledDEBstd() ... 3 parameter version
+# getE0() [solve eb = f ]
+# runDEBstd(f, p::LongParams, timespan)
+  ## first runs getE0, then runs model for some multiple of time at birth, plots l(t)
 
-function scaledDEBstd(du, u, p::ShortParams, t)  # eqns 2.26 to 2.28, DEB book
+function scaledDEBstd(du, u, p::ShortParams, t)  # eqns 2.26 to 2.28, DEB book 3rd ed
     # version for getlbDEBstd
     g, kap, k  = p
     uE, l, uH = u
     du[1] = -uE*l^2 * (g+l)/(uE+l^3)
     du[2] = (1.0/3.0)*(g*uE - l^4)/(uE + l^3)
-    du[3] = (1-kap) * uE*l^2 * (g+l)/(uE+l^3) - k*uHend
-end # module
+    du[3] = (1-kap) * uE*l^2 * (g+l)/(uE+l^3) - k*uH
+  end
 
-function scaledDEBstd(du, u, p::LongParams, t)  # eqns 2.26 to 2.28, DEB book
+function scaledDEBstd(du, u, p::LongParams, t)  # eqns 2.26 to 2.28, DEB book 3rd ed
     # version for getlbDEBstd
     g, kap, k, lT, f, uHb, uHp  = p
     uE, l, uH = u
