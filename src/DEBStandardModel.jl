a363ed44d06117b470d66b40cdb18cde36b0b8bf
HenriLaurie <henri.laurie@gmail.com>
2021-12-09 13:12:21 +0200

    Minimum  working model done

--- src/DEBStandardModel.jl
+++ src/DEBStandardModel.jl
@@ -15,29 +15,15 @@ module DEBStandardModel
 
   using DifferentialEquations, Plots, DelimitedFiles
 
-  struct ShortParams # reduced form suitable for solving for E0.
+  struct DEBstd  # specifies std model
+    ## matching of function to u0 and parameters rests on user
+    fname::Function
+    growthparams::Vector{Float64}
+    maturityparams::Vector{Float64}
+    u0::Vector{Float64} 
   end
 
-  struct stdParams  # growth only (no reproduction or mortality or respiration etc) 
-  f; g; kap; k; lT; feedingyes; adultno
-  end 
-
-  struct LongParams # contains the full problem
-  growthparams::stdParams
-  maturityparams::Vector{Float64}
-  uE0::Float64
-  end
-
-  function scaledDEBstd(du, u, p::ShortParams, t)  # eqns 2.26 to 2.28, DEB book 3rd ed
-    # this version only works for embryo phase; it is used to determine tau_b 
-    g, kap, k  = p
-    uE, l, uH = u
-    du[1] = -uE*l^2 * (g+l)/(uE+l^3)
-    du[2] = (1.0/3.0)*(g*uE - l^4)/(uE + l^3)
-    du[3] = (1-kap) * uE*l^2 * (g+l)/(uE+l^3) - k*uH
-  end
-
-  function scaledDEBstd(du, u, p::stdParams, t)  # eqns 2.26 to 2.28, DEB book 3rd ed
+  function stdDEBscaled(du, u, p, t)  # eqns 2.26 to 2.28, DEB book 3rd ed
     # requires the parameter uE0 for initial condition
     # and juvreached better reset via callback at tau_b
     # and adultreached via callback on uH 
@@ -49,46 +35,44 @@ module DEBStandardModel
     du[3] = (1-adultreached)*((1-kap) * uE*l^2 * (g+l)/(uE+l^3) - k*uH )
   end
 
-  function getscaledparams(fileAmPparsCSV)
+function makeparsdict(csvfilename)
+  lines = readdlm(csvfilename,',') 
+  parsdict = Dict(lines[i,1] => lines[i,2] for i = 1:size(lines)[1])
+  return parsdict
+  end
+
+  function packDEBscaled_std(parsdict, fname = stdDEBscaled)
     # IN: 
-    ## fileAmPparsCSV      string giving (path?) filename with an AmP name, value per row
+    ## parsdict     Dict, keys are AmP symbols (as strings)
+    ## 
     #
     # OUT:
-    ## p::LongParams
+    ## p::DEBstd
 
-    #read file to dictionary
-    #= 
-     I decided to standardise on Add-my-Pet symbols for parameter names.
-     Hence the long list below, I don't know how to make a name from a string.
-     
-     Still a problem with f, as AmP gives a constant but I'd like it to be 
-     easily specified without rerunning this code.
-     A problem to be solved later, here I just set f to 1.
-    =#
-    lines = readdlm(fileAmPparsCSV,',') 
-    parsdict = Dict(lines[i,1] => lines[i,2] for i = 1:size(lines)[1])
-    T_ref = parsdict("T_ref")
-    E_Hb = parsdict( "E_Hb")
-    s_G = parsdict( "s_G")
-    p_Am = parsdict("p_Am")
-    T_body = parsdict("T_body")
-    E_Hp = parsdict("E_Hp")
-    p_M = parsdict("p_M")
-    T_A = parsdict("T_A")
-    z = parsdict("z")
-    kap_P = parsdict("kap_P")
-    h_a = parsdict("h_a")
-    v = parsdict("v")
-    E_G = parsdict("E_G")
-    kap_R = parsdict("kap_R")
-    kap_X = parsdict("kap_X")
-    k_J = parsdict("k_J")
-    F_m = parsdict("F_m")
-    p_T = parsdict("p_T")
-    kap = parsdict("kap")
+    #unpack parsdict to parameter names
+    T_ref = parsdict["T_ref"]
+    E_Hb = parsdict[ "E_Hb"]
+    s_G = parsdict[ "s_G"]
+    p_Am = parsdict["p_Am"]
+    T_body = parsdict["T_body"]
+    E_Hp = parsdict["E_Hp"]
+    p_M = parsdict["p_M"]
+    T_A = parsdict["T_A"]
+    z = parsdict["z"]
+    kap_P = parsdict["kap_P"]
+    h_a = parsdict["h_a"]
+    v = parsdict["v"]
+    E_G = parsdict["E_G"]
+    kap_R = parsdict["kap_R"]
+    kap_X = parsdict["kap_X"]
+    k_J = parsdict["k_J"]
+    F_m = parsdict["F_m"]
+    p_T = parsdict["p_T"]
+    kap = parsdict["kap"]  
+    f   = parsdict["f"]
+    E0  = parsdict["E0"]
 
-    #compute and pack scaled parmeters into LongParams object
-    f   = 1.0
+    # compute derived parameters
     k_M = p_M/E_G 
     E_m = p_Am/v    
     g   = E_G/(kap*E_m)  
@@ -101,10 +85,12 @@ module DEBStandardModel
     uHp = E_Hp/(g*E_m*L_m^3) 
     uE0 = E0/(g*E_m*L_m^3)
 
-    growthpars = stdParams(f, g, kap, k, lT, feedingyes, adultno)
+    #pack the relevant vectors
+    growthpars   = [f, g, kap, k, lT, feedingyes, adultno]
     maturitypars = [uHb, uHp]
+    u0           = [uE0, 0.0, 0.0]  
   
-    return LongParams(growthpars, maturitypars, uE0) 
+    return DEBstd(fname, growthpars, maturitypars, u0) 
     
   end
   ## Hons course version: stdAmPparams = [f, g, kap, k, lT, feedingyes, adultno] 
@@ -114,13 +100,13 @@ module DEBStandardModel
   ## maybe make LongParams named tuple (stdAmParams, maturitylevels, uE0)
   ## and have a dummy/output function that sets up problem, solves it, plots l(t)
 
-function output(params::LongParams, tspan)
-  stdAmPparams, maturitylevels, uE0 = params  
-  u0 = [uE0, 0.0, 0.0]
-  prob = ODEProblem(scaledDEBstd, u0, stdAmPparams, t)
+function output(p::DEBstd, tspan)
+  show(tspan); println("In output")
+  f, growthpars, maturitypars, u0 = p.fname, p.growthparams, p.maturityparams, p.u0  
+  prob = ODEProblem(f, u0, tspan, growthpars)
 
   #create stdDEBcallback
-  uhb, uhp = maturitylevels
+  uhb, uhp = maturitypars
   function reached_uHb(u, t, integrator)     u[3] - uhb         end
   function startfeeding!(integrator)      
     integrator.p[6] = 1.0 
@@ -133,20 +119,22 @@ function output(params::LongParams, tspan)
   stdDEBcallback = CallbackSet(cbonuHb, cbonuHp)
 
   #solve and plot
-  sol = solve(prob, stdDEBcallback)     
+  sol = solve(prob, cb=stdDEBcallback)     
   plot(sol, vars=2 )
   # plot(title = "labelslater" ) #maybe add timestamp?
 
   end
 
 function rundemo(datafile, tspan) # and another method for f as function? 
-  runparams = getscaledparams(datafile)
+  parsdict = makeparsdict(datafile)
+ show(tspan) 
+  runparams = packDEBscaled_std(parsdict)
   output(runparams, tspan)
   end
 
-# test: rundemom("Msplendens_AmPfittedparams.csv", [0.0, 20.0]) should produce a specific figure
+# test: rundemo("Msplendens_AmPfittedparams.csv", [0.0, 20.0]) should produce a specific figure
 
-  #end # module
+end # module
 
 #=
 NOTES
